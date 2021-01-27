f6eef433a2f51e0d3aa6f31f3908c65c6a9bdc09
Anxo <areymasid@gmail.com>
2021-01-27 01:38:56 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,13 @@
 ''' Agent '''
+
+# import libraries, variables and classes
+import numpy
+from game_parameters import *
+from state import State
+
+# define Agent class
+class Agent:
+    def __init_(self):
+        self.State = State()
+        self.states = []
+        self.actions = ['up', 'down', 'right', 'left']
