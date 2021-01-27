596cc5d551e770dfc0e146ba44c8183ee4df2c62
Anxo <areymasid@gmail.com>
2021-01-27 02:16:40 +0100

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,16 @@
+''' Game Parameters '''
+
+# board dimensions
+BOARD_LENGTH = 5
+BOARD_WIDTH = 5
+
+# game key positions
+START = (1, 1)
+WIN = (2, 3)
+LOSE = (3, 2)
+OBSTACLES = [(1, 3), (3, 1)]
+
+# options
+INPUT_STEP = True
+EXPLORATION_RATE = 0.3
+LEARNING_RATE = 0.3
