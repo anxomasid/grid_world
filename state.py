c702ecd25feb3c18c3db7fca4e9c1b55ae1df758
Anxo <areymasid@gmail.com>
2021-01-27 01:39:01 +0100

    Update state.py

--- state.py
+++ state.py
@@ -1,13 +1,17 @@
 ''' State '''
 
+# import libraries and variables
 import numpy
 from game_parameters import *
 
+# define State class
 class State:
-    def __init__(self, state=START):
+
+    def __init__(self, state=START, input_step=INPUT_STEP):
         self.board = numpy.zeros(BOARD_LENGTH, BOARD_WIDTH)
         self.state = state
         self.end = False
+        self.inputStep = INPUT_STEP
 
     def valueReward(self):
         if self.state == WIN:
@@ -20,6 +24,16 @@ class State:
             return 0
 
     def nextPosition(self, action):
-        asdf
+        'Falta checkear OBSTACLES y INPUT_STEP'
+        if action == 'up':
+            nextState = (self.State[0], self.State[1] + 1)
+        elif action == 'down':
+            nextState = (self.State[0], self.State[1] - 1)
+        elif action == 'right':
+            nextState = (self.State[0] + 1, self.State[1])
+        elif action == 'left':
+            nextState = (self.State[0] - 1, self.State[1])
+        return nextState
 
     def plotBoard(self):
+        'Falta función de plot'
