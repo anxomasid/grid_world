7aee1df7fe4956d3a145df80bdacf2653ded7577
Anxo <areymasid@gmail.com>
2021-01-29 02:28:07 +0100

    Update state.py

--- state.py
+++ state.py
@@ -6,56 +6,58 @@ from parameters import *
 class State:
 
     def __init__(self, state=STATE_START, deterministic=DETERMINISTIC):
-        self.board = numpy.zeros(BOARD_LENGTH, BOARD_WIDTH)
+        self.board = numpy.zeros([BOARD_WIDTH, BOARD_LENGTH])
         self.state = state
         self.endGame = False
         self.deterministic = DETERMINISTIC
 
     def valueReward(self):
-        if self.state == WIN:
+        if self.state == STATE_WIN:
             self.endGame = True
             return 1
-        elif self.state == LOSE:
+        elif self.state == STATE_LOSE:
             self.endGame= True
             return -1
         else:
             return 0
 
     def nextPosition(self, action):
+        nextState = (0, 0)
         if self.deterministic:
-            if action == 'up':
-                nextState = (self.state[0], self.state[1] + 1)
-            elif action == 'down':
-                nextState = (self.state[0], self.state[1] - 1)
-            elif action == 'right':
-                nextState = (self.state[0] + 1, self.state[1])
-            elif action == 'left':
+            if action == "up":
                 nextState = (self.state[0] - 1, self.state[1])
-            elif action == 'up-right':
-                nextState = (self.state[0] + 1, self.state[1] + 1)
-            elif action == 'up-left':
-                nextState = (self.state[0] + 1, self.state[1] - 1)
-            elif action == 'down-right':
-                nextState = (self.state[0] + 1, self.state[1] - 1)
-            elif action == 'down-left':
+            if action == "down":
+                nextState = (self.state[0] + 1, self.state[1])
+            if action == "left":
+                nextState = (self.state[0], self.state[1] - 1)
+            if action == 'right':
+                nextState = (self.state[0], self.state[1] + 1)
+            if action == 'up-right':
+                nextState = (self.state[0] - 1, self.state[1] + 1)
+            if action == 'up-left':
                 nextState = (self.state[0] - 1, self.state[1] - 1)
-            if (nextState[0] >= 1) and (
-                nextState[0] <= (BOARD_LENGTH)) and (
-                nextState[1] >= 1) and (
-                nextState[1] <= (BOARD_WIDTH)) and (
-                nextState not in STATE_OBSTACLES):
-                return nextState
+            if action == 'down-right':
+                nextState = (self.state[0] + 1, self.state[1] + 1)
+            if action == 'down-left':
+                nextState = (self.state[0] + 1, self.state[1] - 1)
+        if (nextState[0] >= 0) and (
+            nextState[0] <= (BOARD_WIDTH - 1)) and (
+            nextState[1] >= 0) and (
+            nextState[1] <= (BOARD_LENGTH - 1)) and (
+            nextState not in  STATE_OBSTACLES):
+            return nextState
         return self.state
 
     def plotBoard(self):
         self.board[self.state] = 0.1
-        self.board[WIN_STATE] = 1
-        self.board[LOSE_STATE] = -1
-        self.board[(1, 1)] = -0.1
-        for i in range(1, BOARD_WIDTH + 1):
+        self.board[STATE_WIN] = 1
+        self.board[STATE_LOSE] = -1
+        for i in STATE_OBSTACLES:
+            self.board[i] = -0.1
+        for i in range(BOARD_WIDTH):
             print('----' * BOARD_LENGTH + '-')
             out = '| '
-            for j in range(0, BOARD_LENGTH):
+            for j in range(BOARD_LENGTH):
                 if self.board[i, j] == 0.1:
                     token = 'O'
                 if self.board[i, j] == 1:
@@ -68,4 +70,4 @@ class State:
                     token = ' '
                 out += token + ' | '
             print(out)
-        print('----' * BOARD_BOARD_LENGTH + '-')
+        print('----' * BOARD_LENGTH + '-')
