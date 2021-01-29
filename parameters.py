4cd3da77e497ab940111b6262f48d96b6c90a636
Anxo <areymasid@gmail.com>
2021-01-29 02:28:01 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,16 +1,13 @@
 ''' Parameters '''
 
-# board dimensions
-BOARD_LENGTH = 5
+BOARD_LENGTH = 7
 BOARD_WIDTH = 5
 
-# game key states
 STATE_START = (1, 1)
-STATE_WIN = (2, 3)
+STATE_WIN = (4, 6)
 STATE_LOSE = (3, 2)
-STATE_OBSTACLES = [(1, 3), (3, 1)]
+STATE_OBSTACLES = [(4, 3), (1, 3)]
 
-# options
 DETERMINISTIC = True
 EXPLORATION_RATE = 0.3
 LEARNING_RATE = 0.3
